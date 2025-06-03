0666f2eaa5cf7664cbc87a8c02ae905819d76086
Gia Izyumskiy <94987011+GiaI14@users.noreply.github.com>
2025-06-03 16:01:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -65,6 +65,7 @@ app.use(csrf());
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'images')));
 
 // Helmet CSP middleware with dynamic nonce
 app.use((req, res, next) => {
