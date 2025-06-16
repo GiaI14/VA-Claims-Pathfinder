6629bf3c417f8737e72df6136dbef8e4df15b16a
Gia Izyumskiy <94987011+GiaI14@users.noreply.github.com>
2025-06-16 11:46:23 -0500

    Update contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { transporter } = require('./auth'); // adjust path if needed
-
+const { transporter } = require('./auth'); 
 router.get('/contact', (req, res) => {
   res.render('contact', {
     csrfToken: req.csrfToken(),
