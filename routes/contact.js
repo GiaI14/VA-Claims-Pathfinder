dfb0000398f113f24fc45ce7ff63f131a1b17b1c
Gia Izyumskiy <94987011+GiaI14@users.noreply.github.com>
2025-06-16 11:26:48 -0500

    Update contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { transporter } = require('../auth'); // adjust path if needed
+const { transporter } = require('./auth'); // adjust path if needed
 
 router.post('/contact', async (req, res) => {
   const { name, email, phone, message } = req.body;
