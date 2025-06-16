fe307741784bff6ad71306f391e526836e794458
Gia Izyumskiy <94987011+GiaI14@users.noreply.github.com>
2025-06-16 12:02:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const registrationRoutes = require('./routes/registration');
 const symptomRoutes = require('./routes/symptomRoutes');
 const secondaryConditionRoutes = require('./routes/secondaryConditionRoutes');
 const authRoutes = require('./routes/auth');
+const contactRouter = require('./routes/contact'); //test
 
 dotenv.config();
 
@@ -171,6 +172,7 @@ app.use(registrationRoutes);
 app.use('/', symptomRoutes);
 app.use('/api', secondaryConditionRoutes);
 app.use('/auth', authRoutes);
+app.use('/', contactRouter); //test
 
 // Main routes
 app.get('/', (req, res) => {
