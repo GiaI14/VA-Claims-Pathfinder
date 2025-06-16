febae4f2e2ce70e53480bccb9d914c12fb2f0f16
Gia Izyumskiy <94987011+GiaI14@users.noreply.github.com>
2025-06-16 18:35:43 -0500

    Update contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -49,7 +49,7 @@ router.post('/', async (req, res) => {
     const info = await transporter.sendMail(mailOptions);
     console.log('Email sent:', info.messageId);
 
-    res.redirect('/?message=sent');
+    res.redirect('/?message=success');
   } catch (err) {
     console.error('Error in /contact route:', err);
     res.status(500).json({ message: 'Internal Server Error', error: err.message });
