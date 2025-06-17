ac010f861cfa4054f413a472b79e0b44ec3e6e46
Gia Izyumskiy <94987011+GiaI14@users.noreply.github.com>
2025-06-17 12:09:52 -0500

    Update server.js

--- server.js
+++ server.js
@@ -182,7 +182,7 @@ app.get('/', (req, res) => {
     res.render('index', {
       csrfToken: req.csrfToken(),
       nonce: res.locals.nonce,
-      message: req.query.message || null
+      flash: req.query.flash || null
     });
   } catch (err) {
     console.error('Error in GET / route:', err);
