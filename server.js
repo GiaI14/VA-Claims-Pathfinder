90c94d0a18f29e878da1fd36e680ce29b3c5321e
Gia Izyumskiy <94987011+GiaI14@users.noreply.github.com>
2025-06-17 10:59:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -181,7 +181,7 @@ app.get('/', (req, res) => {
   res.render('index', { 
     csrfToken: req.csrfToken(), 
     nonce: res.locals.nonce });
-    message: req.query.message
+    message: req.query.message || null,
   });
 });
 
