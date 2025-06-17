99a30808f174eb8540466ec69219ad474c380f10
Gia Izyumskiy <94987011+GiaI14@users.noreply.github.com>
2025-06-17 12:35:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -126,7 +126,6 @@ app.use((req, res, next) => {
 // Middleware to pass CSRF token to all views
 app.use((req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
-  res.locals.flash = req.query.flash || null;
   next();
 });
 
