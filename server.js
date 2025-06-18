2bd3aecc60ba924ef96ce0071a00bf5b18d58289
Gia Izyumskiy <94987011+GiaI14@users.noreply.github.com>
2025-06-18 15:59:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -62,12 +62,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'images')));
 
 // Helmet CSP middleware with dynamic nonce
+app.use(helmet());
+
 app.use((req, res, next) => {
   const nonce = res.locals.nonce;
+  const nonceString = `'nonce-${nonce}'`; 
+
   helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", `"nonce-${nonce}"`, "https://accounts.google.com", "https://apis.google.com"],
+      scriptSrc: ["'self'", nonceString, "https://accounts.google.com", "https://apis.google.com"],
       styleSrc: ["'self'", "'unsafe-inline'", "https://accounts.google.com"],
       frameSrc: ["'self'", "https://accounts.google.com"],
       connectSrc: ["'self'", "https://accounts.google.com", "https://play.google.com"],
