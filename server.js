1faff17f8f50d507513c7bdeed0b639f29e70932
Gia Izyumskiy <94987011+GiaI14@users.noreply.github.com>
2025-07-08 17:25:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -78,6 +78,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(flash()); //added 
 
+app.use('/api', calculatorRoutes);
+app.use('/api', secondaryConditionRoutes);
+
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'images')));
@@ -155,10 +158,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Routes
-app.use('/api', calculatorRoutes);
 app.use(registrationRoutes);
 app.use('/', symptomRoutes);
-app.use('/api', secondaryConditionRoutes);
 app.use('/auth', authRoutes);
 //app.use('/', contactRoutes);
 
