8c198a800d0a2c04595e845a649306b53c86e06b
Gia Izyumskiy <94987011+GiaI14@users.noreply.github.com>
2025-08-07 13:02:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,24 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
+const systemImages = {
+  'Dental and Oral Conditions': '512px-202402_Oral_Cavity.svg.png',
+  'Hemic and Lymphatic Systems': '512px-2201_Anatomy_of_the_Lymphatic_System.jpg',
+  'Cardiovascular system': '512px-Circulatory_System_en_edited.svg.png',
+  'Skin': '512px-Dermatology_-_Integumentary_system_1_--_Smart-Servier.png',
+  'Digestive System': '512px-Digestive_system_diagram_en.svg.png',
+  'Endocrine system': '512px-Endocrine_English.svg.png',
+  'Gynecological conditions and disprders of the breast': '512px-Female_genital_system_-_Sagittal_view.svg.png',
+  'Eye': '512px-Lateral_orbit_nerves_chngd.jpg',
+  'Genitourinary system': '512px-Male_and_female_genital_organs.svg.png',
+  'Musculoskeletal system': '512px-Muscles_front_and_back-es.png',
+  'Respiratory system': '512px-Respiratory_system_complete_fr_simplified.svg.png',
+  'Nervous System': '512px-TE-Nervous_system_diagram.svg.png',
+  'Ear': 'Auditory_System.jpg',
+  'Sense Organs': 'Sense-Organ.png',
+};
+
+
 const sessionStore = new MySQLStore({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
