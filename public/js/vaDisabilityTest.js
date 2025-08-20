7ab8149028d8b968e5c89ff27d34b79feadd278b
Gia Izyumskiy <94987011+GiaI14@users.noreply.github.com>
2025-08-20 11:28:45 -0500

    Update vaDisabilityTest.js

--- public/js/vaDisabilityTest.js
+++ public/js/vaDisabilityTest.js
@@ -188,12 +188,12 @@ document.addEventListener('DOMContentLoaded', () => {
       `).join('<hr>');
 
       div.innerHTML = `
-        <h3>System: ${entry.system} | Sub-System: ${entry.subSystem}</h3>
+        <h3>System: ${entry.system}</h3>
         ${conditionsHtml}
       `;
     } else {
       div.innerHTML = `
-        <h3>System: ${entry.system} | Sub-System: ${entry.subSystem}</h3>
+        <h3>System: ${entry.system}</h3>
         <p>${entry.message || 'No matching conditions found.'}</p>
       `;
     }
