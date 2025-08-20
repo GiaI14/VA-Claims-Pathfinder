1b6f9a3f67b94eae9a43f525f744a82d712bb40d
Gia Izyumskiy <94987011+GiaI14@users.noreply.github.com>
2025-08-20 16:56:43 -0500

    Update vaDisabilityTest.js

--- public/js/vaDisabilityTest.js
+++ public/js/vaDisabilityTest.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const symptomEntriesContainer = document.getElementById('symptomEntriesContainer');
+  const labels = document.querySelectorAll(".symptom-label");
   const analyzeButton = document.getElementById('analyzeButton');
   const resultsDiv = document.getElementById('results');
 
@@ -48,6 +49,15 @@ document.addEventListener('DOMContentLoaded', () => {
   lightboxImg?.addEventListener('click', closeLightbox);
   closeBtn?.addEventListener('click', closeLightbox);
 
+
+  function updateLabels() {
+  if (systemDropdown.value && subSystemDropdown.value) {
+    labels.forEach(label => label.classList.add("bold-label"));
+  } else {
+    labels.forEach(label => label.classList.remove("bold-label"));
+  }
+}
+  
   // ------------------- TOGGLE INPUT METHOD -------------------
   function toggleSymptomInput(entry, value) {
     const typingInput = entry.querySelector('.typed-symptoms');
