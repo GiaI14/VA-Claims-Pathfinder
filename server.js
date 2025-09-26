f6c8173e5db2b8e0091764fda901fe590735b0da
Gia Izyumskiy <94987011+GiaI14@users.noreply.github.com>
2025-09-26 10:17:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -86,8 +86,8 @@ app.use(flash()); //added
 const csrfProtection = csrf();
 app.use(csrfProtection); 
 
+app.use('/api', calculatorRoutes); 
 app.use('/api', secondaryConditionRoutes);
-app.use('/api', contactRoutes);
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
