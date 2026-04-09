48f315ec98891a5db189c511db6d2a5de703cd16
Gia Izyumskiy <94987011+GiaI14@users.noreply.github.com>
2026-04-09 10:10:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use((req, res, next) => {
         "'self'",
         `'nonce-${res.locals.nonce}'`,  // IMPORTANT: this string must be wrapped in quotes here
         "https://accounts.google.com",
-        "https://apis.google.com"
+        "https://apis.google.com",
         "https://cdnjs.cloudflare.com"
       ],
       styleSrc: ["'self'", "'unsafe-inline'", "https://accounts.google.com"],
